5dfbfabe419c0fd1d14555a77c7175d5dc33ae3a
RAULEZW <raul_jai_gopal@outlook.com>
2025-08-16 15:34:02 +0800

    Fix formula: install requirements + scripts

--- Formula/yt-downloader.rb
+++ Formula/yt-downloader.rb
@@ -1,5 +1,5 @@
 class YtDownloader < Formula
-  desc "Simple YouTube downloader with Flask web UI"
+  desc "YouTube downloader with Flask web UI"
   homepage "https://github.com/RAULEZW/youtube-downloader"
   url "https://github.com/RAULEZW/youtube-downloader/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "6571120e28784a86d6aea59cbcbd0f9ed9c4290509039063660c97535d00aca9"
@@ -9,9 +9,16 @@ class YtDownloader < Formula
   depends_on "ffmpeg"
 
   def install
-    system "pip3", "install", *std_pip_args, "-r", "requirements.txt"
+    venv = libexec/"venv"
+    system "python3", "-m", "venv", venv
+    system "#{venv}/bin/pip", "install", "--upgrade", "pip"
+    system "#{venv}/bin/pip", "install", "-r", "requirements.txt"
+
     bin.install "start_app.sh" => "yt-downloader"
-    bin.install "stop_app.sh"  => "yt-downloader-stop"
+    bin.install "stop_app.sh" => "yt-downloader-stop"
+
+    # Ensure the start script uses this virtualenv
+    inreplace bin/"yt-downloader", /^exec "\$VENV_DIR\/bin\/python"/, "exec #{venv}/bin/python"
   end
 
   test do
